8c2a92b4f669bd2e26e7add41ed21620eaead024
RiveraTheCreator <iriverab1900@alumno.ipn.mx>
2021-12-16 18:47:58 -0600

    Cambios en app.js

--- app.js
+++ app.js
@@ -62,9 +62,13 @@ app.get('/',(req,res)=>{
     return res.render('index',{categorias:categories});
 });
 
-app.get('/detail',(req,res)=> {return res.sendFile(`${publicPath}/views/generic.html`)});
 app.get('/categorias/:id',controllerProjects.listar);
 app.get('/detail/:id',controllerProjects.mostrar);
 app.listen(app.get('port'));
 console.log(`Server on port  ${app.get('port')}`);
 
+//Error 404
+app.use((req,res,next)=>{
+    res.status(404).send('ERROR 404 Ruta no encontrada');
+    next();
+})
